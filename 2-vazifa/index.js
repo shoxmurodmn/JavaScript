51acdaf1b262d5fe223543c15906e05c977f85d0
mamatovmurod8898@gmail.com <islombekqurbonovfd@gmail.com>
2023-04-12 18:21:03 +0500

    raqamlar yig'indisini xisoblash

---
+++ 2-vazifa/index.js
@@ -0,0 +1,10 @@
+let num = 4555;
+let sum = 0;
+let str = num.toString(); // sonni matnga aylantiramiz
+
+for (let i = 0; i < str.length; i++) {
+  let digit = +str[i]; // belgi alohida songa o'tkaziladi
+   sum += digit;
+}
+
+console.log("raqamlar yig'indisi ",sum); 

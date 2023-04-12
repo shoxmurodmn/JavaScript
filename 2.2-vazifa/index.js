37042d5139a00186676fc1858d108920d55c1aa7
mamatovmurod8898@gmail.com <islombekqurbonovfd@gmail.com>
2023-04-12 18:41:02 +0500

    sonlarni o'quvchi dastur

---
+++ 2.2-vazifa/index.js
@@ -0,0 +1,17 @@
+let son = 3201
+let pul = son.toString()
+
+for (let i= 0 ; i < pul.length; i++ ){
+	
+	if(pul[i] == 1) console.log("bir");
+	else if(pul[i] == 2) console.log("ikki");
+	else if(pul[i] == 3) console.log("uch");
+	else if(pul[i] == 4) console.log("to'rt");
+	else if(pul[i] == 5) console.log("besh");
+	else if(pul[i] == 6) console.log("olti");
+	else if(pul[i] == 7) console.log("yetti");
+	else if(pul[i] == 8) console.log("sakkiz");
+	else if(pul[i] == 9) console.log("to'qqiz");
+	else console.log("no'l");
+
+}

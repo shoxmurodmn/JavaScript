5152c97466b21bb1e51876c25ce1bc1aa191227f
mamatovmurod8898@gmail.com <islombekqurbonovfd@gmail.com>
2023-04-12 15:16:31 +0500

    kara jadvali

---
+++ index.js
@@ -0,0 +1,52 @@
+let ikki = 2 ;
+let uch = 2 ;
+let turt = 2 ;
+let besh = 2 ;
+let olti = 2 ;
+let yetti = 2 ;
+let sakkiz = 2 ;
+let tuqiz = 2 ;
+
+for ( let i = 1 ; i <= 10 ; ++i){
+	for(; ikki <= 10 ; ikki++){
+		  natija = 2 * ikki
+	     console.log(`2 x ${ikki} = ${natija}`);
+	}
+
+	for(; uch <= 10 ; uch ++){
+		natija = 3 * uch 
+		console.log(`3 x ${uch} = ${natija}`);
+	}
+	
+	for(; turt <= 10 ; turt++){
+		natija = 4 * turt
+		console.log(`4 x ${turt} = ${natija}`);
+	}
+
+	for(; besh <= 10 ; besh++){
+		natija = 5 * besh
+		console.log(`5 x ${besh} = ${natija}`);
+	}
+
+	for(; olti <= 10 ; olti++){
+		natija = 6 * olti
+		console.log(`6 x ${olti} = ${natija}`);
+	}
+
+	for(; yetti <= 10 ; yetti++){
+		natija = 7 * yetti
+		console.log(`7 x ${yetti} = ${natija}`);
+	}
+
+	for(; sakkiz <= 10 ; sakkiz++){
+		natija = 8 * sakkiz
+		console.log(`8 x ${sakkiz} = ${natija}`);
+	}
+
+	for(; tuqiz <= 10 ; tuqiz++){
+		natija = 9 * tuqiz
+		console.log(`9 x ${tuqiz} = ${natija}`);
+	}
+
+	
+} 

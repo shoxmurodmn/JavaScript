8fd19e641f61eae35159d39ab69a7973a65cd069
mamatovmurod8898@gmail.com <islombekqurbonovfd@gmail.com>
2023-04-12 18:44:51 +0500

    4 xonali sonlarni xonasini sanash

---
+++ 2.3-vazifa/index.js
@@ -0,0 +1,47 @@
+
+let son = 3222
+let pul = son.toString()
+
+			if(pul[0] == 1) var ming = 'birming'
+			else if(pul[0] == 2) var ming ='ikkiming';
+			else if(pul[0] == 3) var ming ='uchming';
+			else if(pul[0] == 4) var ming ="to'rtming";
+			else if(pul[0] == 5) var ming ='beshming';
+			else if(pul[0] == 6) var ming ='oltiming';
+			else if(pul[0] == 7) var ming ='yettiming';
+			else if(pul[0] == 8) var ming ='sakkizming';
+			else if(pul[0] == 9) var ming ="to'qizming";
+
+			if(pul[2] == 1) var yuz = 'biryuz'
+			else if(pul[1] == 2) var yuz ='ikkiyuz';
+			else if(pul[1] == 3) var yuz ='uchyuz';
+			else if(pul[1] == 4) var yuz ="to'rtyuz";
+			else if(pul[1] == 5) var yuz ='beshyuz';
+			else if(pul[1] == 6) var yuz ='oltiyuz';
+			else if(pul[1] == 7) var yuz ='yettiyuz';
+			else if(pul[1] == 8) var yuz ='sakkizyuz';
+			else if(pul[1] == 9) var yuz ="to'qizyuz";
+			else yuz =" ";
+
+			if(pul[2] == 1) var on = "o'n"
+			else if(pul[2] == 2) var on ='ygirma';
+			else if(pul[2] == 3) var on ="o'tiz";
+			else if(pul[2] == 4) var on ="qirq";
+			else if(pul[2] == 5) var on ='ellik';
+			else if(pul[2] == 6) var on ='oltmish';
+			else if(pul[2] == 7) var on ='yetmish';
+			else if(pul[2] == 8) var on ='sakson';
+			else if(pul[2] == 9) var on ="to'qson";
+			else var on =" ";
+
+			if(pul[3] == 1) var bir = 'bir';
+			else if(pul[3] == 2) var bir ='ikki';
+			else if(pul[3] == 3) var bir ='uch';
+			else if(pul[3] == 4) var bir ="to'rt";
+			else if(pul[3] == 5) var bir ='besh';
+			else if(pul[3] == 6) var bir ='olti';
+			else if(pul[3] == 7) var bir ='yetti';
+			else if(pul[3] == 8) var bir ='sakkiz';
+			else if(pul[3] == 9) var bir ="to'qiz";
+			else  var bir =" ";
+			console.log(ming , yuz, on ,bir )

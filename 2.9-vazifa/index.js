af9160c5390cd8508ee5d9b723a964dd95ff5155
mamatovmurod8898@gmail.com <islombekqurbonovfd@gmail.com>
2023-04-28 20:41:11 +0500

    min va max qiymat ayirmasi

--- 2.9-vazifa/index.js
+++ 2.9-vazifa/index.js
@@ -22,3 +22,24 @@ salom = () => {
 salom()
 
 
+let son1 = parseInt(Math.random() * 100)
+let son2 = parseInt(Math.random() * 100)
+let son3 = parseInt(Math.random() * 100)
+let son4 = parseInt(Math.random() * 100)
+let son5 = parseInt(Math.random() * 100)
+let son6 = parseInt(Math.random() * 100)
+urtacha = () => {
+   var bal = Math.max(son1, son2, son3, son4, son5, son6,)
+   console.log(`max ${+bal}`);
+
+   var past = Math.min(son1, son2, son3, son4, son5, son6,)
+   console.log(`min ${past}`);
+
+   natija = bal - past
+   console.log(`max,- min ==> ${bal}-${past} = ${natija}`);
+}
+
+urtacha()
+
+
+
